89e8c19f7abd01af0987f45f78aa66673e9652b4
DALDALNUGURY <72500196+korobopolly@users.noreply.github.com>
2024-12-20 18:05:06 +0900

    20day

--- python.py
+++ python.py
@@ -887,3 +887,77 @@ def solution(triangle):
 #     print(tmp[0].upper())
 # else:
 #     print("?")
+
+# 예전에는 운영체제에서 크로아티아 알파벳을 입력할 수가 없었다. 따라서, 다음과 같이 크로아티아 알파벳을 변경해서 입력했다.
+# 크로아티아 알파벳	변경
+# č	c=
+# ć	c-
+# dž	dz=
+# đ	d-
+# lj	lj
+# nj	nj
+# š	s=
+# ž	z=
+# 예를 들어, ljes=njak은 크로아티아 알파벳 6개(lj, e, š, nj, a, k)로 이루어져 있다. 단어가 주어졌을 때, 몇 개의 크로아티아 알파벳으로 이루어져 있는지 출력한다.
+# dž는 무조건 하나의 알파벳으로 쓰이고, d와 ž가 분리된 것으로 보지 않는다. lj와 nj도 마찬가지이다. 위 목록에 없는 알파벳은 한 글자씩 센다.
+
+# input_s = input()
+# table = {'c=':'1', 'c-':'2', 'dz=':'3', 'd-':'4'
+#         , 'lj':'5', 'nj':'6', 's=':'7', 'z=':'8'}
+
+# for k,v in table.items():
+#     input_s = input_s.replace(k,v)
+# print(len(input_s))
+
+# 그룹 단어란 단어에 존재하는 모든 문자에 대해서, 각 문자가 연속해서 나타나는 경우만을 말한다. 예를 들면, ccazzzzbb는 c, a, z, b가 모두 연속해서 나타나고, kin도 k, i, n이 연속해서 나타나기 때문에 그룹 단어이지만, aabbbccb는 b가 떨어져서 나타나기 때문에 그룹 단어가 아니다.
+# 단어 N개를 입력으로 받아 그룹 단어의 개수를 출력하는 프로그램을 작성하시오.
+
+# N = int(input().strip())  # 단어 개수
+# count = 0  # 그룹 단어 개수
+
+# for _ in range(N):
+#     word = input().strip()
+#     seen = set()      # 이미 등장한 문자
+#     last_char = None  # 이전 문자
+#     is_group_word = True  # 그룹 단어 여부
+
+#     for char in word:
+#         # 이전 문자와 다른 새 문자가 등장했을 때
+#         if char != last_char:
+#             # 이미 등장한 문자가 다시 나타나면 그룹 단어 아님
+#             if char in seen:
+#                 is_group_word = False
+#                 break
+#             seen.add(char)  # 새로운 문자를 seen에 추가
+#         last_char = char  # 현재 문자를 이전 문자로 갱신
+
+#     if is_group_word:
+#         count += 1
+
+# print(count)
+
+# 인하대학교 컴퓨터공학과를 졸업하기 위해서는, 전공평점이 3.3 이상이거나 졸업고사를 통과해야 한다. 그런데 아뿔싸, 치훈이는 깜빡하고 졸업고사를 응시하지 않았다는 사실을 깨달았다!
+# 치훈이의 전공평점을 계산해주는 프로그램을 작성해보자.
+# 전공평점은 전공과목별 (학점 × 과목평점)의 합을 학점의 총합으로 나눈 값이다.
+# 인하대학교 컴퓨터공학과의 등급에 따른 과목평점은 다음 표와 같다.
+# A+	4.5
+# A0	4.0
+# B+	3.5
+# B0	3.0
+# C+	2.5
+# C0	2.0
+# D+	1.5
+# D0	1.0
+# F	0.0
+# P/F 과목의 경우 등급이 P또는 F로 표시되는데, 등급이 P인 과목은 계산에서 제외해야 한다.
+# 과연 치훈이는 무사히 졸업할 수 있을까?
+
+unit = 0
+major_rating = 0
+for _ in range(20):
+    list_score = input().split()
+    subject_rate = {'A+':4.5, 'A0':4.0,'B+':3.5,'B0':3.0,'C+':2.5,'C0':2.0,'D+':1.5,'D0':1.0,'F':0.0}
+    if list_score[2] != 'P':
+        unit += float(list_score[1])
+        major_rating += float(list_score[1]) * subject_rate[list_score[2]]
+print(major_rating/unit)
