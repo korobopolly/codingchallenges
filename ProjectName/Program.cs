c5d8110b882e233d8c48f38d8b07f2222f306263
korobopolly <shqkd1492@gmail.com>
2025-01-14 10:54:44 +0900

    C# 환경설정

---
+++ ProjectName/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        
+        // 사용자 입력 받기
+        Console.Write("이름을 입력하세요: ");
+        string name = Console.ReadLine();
+        
+        Console.WriteLine($"안녕하세요, {name}님!");
+    }
+}
