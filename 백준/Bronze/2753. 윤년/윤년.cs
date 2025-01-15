b4524c610ca42a9b7b2ad806a185e49b3d4702fc
DALDALNUGURY <72500196+korobopolly@users.noreply.github.com>
2025-01-16 00:16:41 +0900

    [Bronze V] Title: 윤년, Time: 48 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int year = int.Parse(Console.ReadLine());
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+        {
+            Console.WriteLine("1");
+        }
+        else
+        {
+            Console.WriteLine("0");
+        }
+    }
+}
