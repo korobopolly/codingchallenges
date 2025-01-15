907e640c29c87fc5759998347dcdfc1e126b315e
DALDALNUGURY <72500196+korobopolly@users.noreply.github.com>
2025-01-15 17:30:31 +0900

    [Bronze V] Title: 나머지, Time: 52 ms, Memory: 5516 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var input = Console.ReadLine().Split();
+        int A = int.Parse(input[0]);
+        int B = int.Parse(input[1]);
+        int C = int.Parse(input[2]);
+        Console.WriteLine((A + B) % C);
+        Console.WriteLine(((A % C) + (B % C)) % C);
+        Console.WriteLine((A * B) % C);
+        Console.WriteLine(((A % C) * (B % C)) % C);
+    }
+}
