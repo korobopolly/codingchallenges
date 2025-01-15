971100f6b78eea230d543d75b23ccd272ad723f3
DALDALNUGURY <72500196+korobopolly@users.noreply.github.com>
2025-01-15 17:24:33 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 52 ms, Memory: 5548 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var input = Console.ReadLine();
+        Console.WriteLine($"{int.Parse(input)- 543}");
+    }
+}
