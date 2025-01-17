7f45f4d098c1084a9b4771781a3aacad5e176b16
DALDALNUGURY <72500196+korobopolly@users.noreply.github.com>
2025-01-18 00:00:09 +0900

    [Bronze V] Title: 구구단, Time: 52 ms, Memory: 5820 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        for (int i = 1; i < 10; i++)
+        {
+            Console.WriteLine($"{N} * {i} = {N*i}");
+        }
+    }
+}
