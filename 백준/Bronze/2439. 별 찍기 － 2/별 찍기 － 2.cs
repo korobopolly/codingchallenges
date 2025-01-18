ee2acd227f21a3accbe47fc9cfd27e801a64d761
DALDALNUGURY <72500196+korobopolly@users.noreply.github.com>
2025-01-18 13:51:20 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 52 ms, Memory: 5548 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+using System.Text; // StringBuilder를 사용하기 위해 필요
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        StringBuilder sb = new StringBuilder();
+        int N = int.Parse(Console.ReadLine());
+        for (int i = 0; i < N; i++) {
+            sb.Append(new string(' ', N - i - 1));
+            sb.Append(new string('*', i + 1));
+            sb.AppendLine();
+        }
+        Console.WriteLine(sb.ToString());
+    }
+}
