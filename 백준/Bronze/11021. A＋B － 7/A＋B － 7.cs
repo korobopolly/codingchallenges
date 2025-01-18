25530a265088d788022ad79d3376b14bdba382fd
DALDALNUGURY <72500196+korobopolly@users.noreply.github.com>
2025-01-18 13:35:34 +0900

    [Bronze V] Title: A+B - 7, Time: 56 ms, Memory: 5844 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+using System.Text; // StringBuilder를 사용하기 위해 필요
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        StringBuilder sb = new StringBuilder();
+        int N = int.Parse(Console.ReadLine());
+        for (int i = 0; i < N; i++) {
+            var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            sb.AppendLine($"Case #{i+1}: {(input[0] + input[1])}");
+        }
+        Console.WriteLine(sb.ToString());
+    }
+}
