cf7aa3df6187c58b930f420bb2366c0d4cad4f87
DALDALNUGURY <72500196+korobopolly@users.noreply.github.com>
2025-01-21 01:04:33 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 52 ms, Memory: 5528 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic; //HashSet을 사용하기 위해 필요
+using System.Linq; //Linq를 사용하기 위해 필요
+using System.Text; // StringBuilder를 사용하기 위해 필요
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        StringBuilder sb = new StringBuilder();
+        var input = Console.ReadLine();
+        for (int i = (int)'a'; i < (int)'z' + 1; i++) {
+            sb.Append($"{input.IndexOf((char)i)} ");
+        }
+        Console.WriteLine(sb.ToString());
+    }
+}
