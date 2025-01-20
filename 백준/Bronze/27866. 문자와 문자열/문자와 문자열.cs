897c9f6e9262a74c418c17b87d80e392a79784d7
DALDALNUGURY <72500196+korobopolly@users.noreply.github.com>
2025-01-20 18:45:06 +0900

    [Bronze V] Title: 문자와 문자열, Time: 52 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic; //HashSet을 사용하기 위해 필요
+using System.Linq; //Linq를 사용하기 위해 필요
+using System.Text; // StringBuilder를 사용하기 위해 필요
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        StringBuilder sb = new StringBuilder();
+        var input = Console.ReadLine();
+        int N = int.Parse(Console.ReadLine());
+        sb.Append(input[N-1]);
+        Console.WriteLine(sb.ToString());
+    }
+}
