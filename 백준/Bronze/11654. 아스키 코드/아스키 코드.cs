54976f6448dd5cde22a183b02585c85773f4dcc9
DALDALNUGURY <72500196+korobopolly@users.noreply.github.com>
2025-01-20 19:06:24 +0900

    [Bronze V] Title: 아스키 코드, Time: 48 ms, Memory: 5404 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic; //HashSet을 사용하기 위해 필요
+using System.Linq; //Linq를 사용하기 위해 필요
+using System.Text; // StringBuilder를 사용하기 위해 필요
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        StringBuilder sb = new StringBuilder();
+        char input = Console.ReadLine()[0];
+        sb.Append((int)input);
+        Console.WriteLine(sb.ToString());
+    }
+}
