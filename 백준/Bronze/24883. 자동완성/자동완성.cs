9a19a810732e2478643ebff4594825de0b0a2a94
DALDALNUGURY <72500196+korobopolly@users.noreply.github.com>
2025-02-20 23:46:56 +0900

    [Bronze V] Title: 자동완성, Time: 48 ms, Memory: 5372 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+
+class Program2
+{
+    static void Main(string[] args)
+    {
+        var input = Console.ReadLine();
+        Console.WriteLine(input == "N" || input == "n" ? "Naver D2" : "Naver Whale");   
+    }
+}
