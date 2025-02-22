81e6193ef0bce56abeca824c2cf286711915acf3
DALDALNUGURY <72500196+korobopolly@users.noreply.github.com>
2025-02-22 18:46:26 +0900

    [Bronze V] Title: AI 선도대학, Time: 48 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+
+class Program2
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        Console.WriteLine(N - 2024);   
+    }
+}
