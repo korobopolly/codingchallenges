b16c36bf3c4a45f616501df1821bff6d2de6a0d1
DALDALNUGURY <72500196+korobopolly@users.noreply.github.com>
2025-02-28 00:02:29 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 44 ms, Memory: 5324 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Claims;
+using System.Text;
+
+class Program2
+{
+    static void Main(string[] args)
+    {
+        Console.Write("비와이");
+    }
+}
