e30e2da402c3bf6961f8c961a0195d422682f560
DALDALNUGURY <72500196+korobopolly@users.noreply.github.com>
2025-03-27 14:30:34 +0900

    [Bronze V] Title: 웰컴, Time: 48 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".  .   .");
+            Console.WriteLine("|  | _ | _. _ ._ _  _");
+            Console.WriteLine("|/\\|(/.|(_.(_)[ | )(/.");
+        }
+    }
+}
